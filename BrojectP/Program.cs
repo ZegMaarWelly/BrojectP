3ba39692e663f56a6d08da09e29fbf7f2a0181ff
1058989 <1058989@hr.nl>
2023-03-13 16:22:22 +0100

    test
    test

--- BrojectP/Program.cs
+++ BrojectP/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("he he he yup!");
-Console.ReadLine();
+﻿public class Program
+{
+
+}
