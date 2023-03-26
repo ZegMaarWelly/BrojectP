6408e7b681e51ff5b1753f827cb969740a32e489
t3xj <124897596+t3xj@users.noreply.github.com>
2023-03-26 21:08:31 +0200

    comit

--- Project/Presentation/ScreeningRoom.cs
+++ Project/Presentation/ScreeningRoom.cs
@@ -1,26 +1,32 @@
 ﻿
 public class ScreeningRoom
 {
+    public static void Main()
+    {
+        screening_room_small();
+    }
     public static void screening_room_small()
     {
-        bool[] test = { true, true, true, true, true, true, true, true};
+        bool[] test = {false, true, true, true, true, true, true, true};
         List<bool> row = new List<bool>(test);
-        string row_start = "##";
+        string first_row_string = "##";
         for (int i = 0; i < 2; i++)
         {
             for (int j = 0; j < 8; j++)
             {
-                if (row[i] = true)
+                if (row[j] == true)
                 {
-                    string row_string = row_string + "O";
-                    Console.WriteLine(row_string);
+                    first_row_string = first_row_string + "O";
+                }
+                else
+                {
+                    first_row_string = first_row_string + "X";
                 }
             }
+            first_row_string = first_row_string + "##";
+            Console.WriteLine(first_row_string);
+            first_row_string = "##";
         }
-            
-        
-
-
     }
 }
 
