8eef081b8958c12cf413644e61c4b97efdf1bff2
marcelboo <101633688+marcelboo@users.noreply.github.com>
2023-03-26 23:50:59 +0200

    Added the option to log in as admin. and then get to the admin menu.

--- Project/Presentation/Menu.cs
+++ Project/Presentation/Menu.cs
@@ -7,17 +7,17 @@ static class Menu
     static public void Start()
     {
         Console.WriteLine("Enter 1 to login");
-        Console.WriteLine("Enter 2 to get to admin thingies");
+        //Console.WriteLine("Enter 2 to get to admin thingies");
 
         string input = Console.ReadLine();
         if (input == "1")
         {
             UserLogin.Start();
         }
-        else if (input == "2")
-        {
-            AdminMenu.Start();
-        }
+        //else if (input == "2")
+        //{
+        //    AdminMenu.Start();
+        //}
         else
         {
             Console.WriteLine("Invalid input");
