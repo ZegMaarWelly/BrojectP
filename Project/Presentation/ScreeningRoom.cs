89042385f03cfadb29882b1f86722bf15dfd332e
t3xj <124897596+t3xj@users.noreply.github.com>
2023-04-02 18:31:26 +0200

    screening room displays

--- Project/Presentation/ScreeningRoom.cs
+++ Project/Presentation/ScreeningRoom.cs
@@ -1,32 +1,174 @@
 ﻿
+using System;
+
 public class ScreeningRoom
 {
     public static void Main()
     {
-        screening_room_small();
+        screening_room_large();
     }
     public static void screening_room_small()
     {
-        bool[] test = {false, true, true, true, true, true, true, true};
-        List<bool> row = new List<bool>(test);
-        string first_row_string = "##";
-        for (int i = 0; i < 2; i++)
+        List<string> screening_room = new List<string>
+                {"14|     O O O O O O O O    ",
+                 "13|   O O O O O O O O O O  ",
+                 "12|   O O O O O O O O O O  ",
+                 "11| O O O O O O O O O O O O",
+                 "10| O O O O O O O O O O O O",
+                 "09| O O O O O O O O O O O O",
+                 "08| O O O O O O O O O O O O",
+                 "07| O O O O O O O O O O O O",
+                 "06| O O O O O O O O O O O O",
+                 "05| O O O O O O O O O O O O",
+                 "04| O O O O O O O O O O O O",
+                 "03|   O O O O O O O O O O  ",
+                 "02|     O O O O O O O O    ",
+                 "01|     O O O O O O O O    ",
+                 "  x————————————————————————",
+                 "    1 2 3 4 5 6 7 8 9 1 1 1",
+                 "                      0 1 2"};
+
+        while (true)
         {
-            for (int j = 0; j < 8; j++)
+            foreach (var test in screening_room)
             {
-                if (row[j] == true)
-                {
-                    first_row_string = first_row_string + "O";
-                }
-                else
-                {
-                    first_row_string = first_row_string + "X";
-                }
+                Console.WriteLine(test);
             }
-            first_row_string = first_row_string + "##";
-            Console.WriteLine(first_row_string);
-            first_row_string = "##";
+            Console.WriteLine("Selecteer een rij:");
+            var input_row = Convert.ToInt32(Console.ReadLine());
+            var row = (screening_room.Count - input_row - 3);
+            Console.WriteLine("Selecteer een stoel:");
+            var input_column = (Convert.ToInt32(Console.ReadLine()) - 1) * 2 + 4;
+            var your_line = screening_room[row];
+            if (your_line[input_column] == Convert.ToChar("O"))
+            {
+                your_line = your_line.Remove(input_column, 1).Insert(input_column, "X");
+                screening_room[row] = your_line;
+                Console.Clear();
+                Console.WriteLine($"U heeft stoel {input_column / 2 - 1} op rij {input_row} geselecteerd, wilt u deze stoel reserveren?");
+                Thread.Sleep(3000);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Deze stoel is niet beschikbaar, kies alstublieft een beschikbare stoel");
+                Thread.Sleep(3000);
+                Console.Clear();
+            }
+
+        }
+    }
+    public static void screening_room_medium()
+    {
+        List<string> screening_room = new List<string>
+                {"19|   O O O O O O O O O O O O O O O O   ",
+                 "18|   O O O O O O O O O O O O O O O O   ",
+                 "17|   O O O O O O O O O O O O O O O O   ",
+                 "16|   O O O O O O O O O O O O O O O O   ",
+                 "15|   O O O O O O O O O O O O O O O O   ",
+                 "14|   O O O O O O O O O O O O O O O O   ",
+                 "13| O O O O O O O O O O O O O O O O O O ",
+                 "12| O O O O O O O O O O O O O O O O O O ",
+                 "11| O O O O O O O O O O O O O O O O O O ",
+                 "10| O O O O O O O O O O O O O O O O O O ",
+                 "09| O O O O O O O O O O O O O O O O O O ",
+                 "08|   O O O O O O O O O O O O O O O O   ",
+                 "07|   O O O O O O O O O O O O O O O O   ",
+                 "06|   O O O O O O O O O O O O O O O O   ",
+                 "05|     O O O O O O O O O O O O O O     ",
+                 "04|     O O O O O O O O O O O O O O     ",
+                 "03|     O O O O O O O O O O O O O O     ",
+                 "02|       O O O O O O O O O O O O       ",
+                 "01|       O O O O O O O O O O O O       ",
+                 "  x————————————————————————-------------",
+                 "    1 2 3 4 5 6 7 8 9 1 1 1 1 1 1 1 1 1 ",
+                 "                      0 1 2 3 4 5 6 7 8 "};
+
+        while (true)
+        {
+            foreach (var test in screening_room)
+            {
+                Console.WriteLine(test);
+            }
+            Console.WriteLine("Selecteer een rij:");
+            var input_row = Convert.ToInt32(Console.ReadLine());
+            var row = (screening_room.Count - input_row - 3);
+            Console.WriteLine("Selecteer een stoel:");
+            var input_column = (Convert.ToInt32(Console.ReadLine()) - 1) * 2 + 4;
+            var your_line = screening_room[row];
+            if (your_line[input_column] == Convert.ToChar("O"))
+            {
+                your_line = your_line.Remove(input_column, 1).Insert(input_column, "X");
+                screening_room[row] = your_line;
+                Console.Clear();
+                Console.WriteLine($"U heeft stoel {input_column / 2 - 1} op rij {input_row} geselecteerd, wilt u deze stoel reserveren?");
+                Thread.Sleep(3000);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Deze stoel is niet beschikbaar, kies alstublieft een beschikbare stoel");
+                Thread.Sleep(3000);
+                Console.Clear();
+            }
+
+        }
+    }
+    public static void screening_room_large()
+    {
+        List<string> screening_room = new List<string>
+                {"20|         O O O O O O O O O O O O O O O O O O O O O O         ",
+                 "19|       O O O O O O O O O O O O O O O O O O O O O O O O       ",
+                 "18|       O O O O O O O O O O O O O O O O O O O O O O O O       ",
+                 "17|       O O O O O O O O O O O O O O O O O O O O O O O O       ",
+                 "16|       O O O O O O O O O O O O O O O O O O O O O O O O       ",
+                 "15|     O O O O O O O O O O O O O O O O O O O O O O O O O O     ",
+                 "14|   O O O O O O O O O O O O O O O O O O O O O O O O O O O O   ",
+                 "13| O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O ",
+                 "12| O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O ",
+                 "11| O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O ",
+                 "10| O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O ",
+                 "09| O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O ",
+                 "08|   O O O O O O O O O O O O O O O O O O O O O O O O O O O O   ",
+                 "07|     O O O O O O O O O O O O O O O O O O O O O O O O O O     ",
+                 "06|     O O O O O O O O O O O O O O O O O O O O O O O O O O     ",
+                 "05|       O O O O O O O O O O O O O O O O O O O O O O O O       ",
+                 "04|       O O O O O O O O O O O O O O O O O O O O O O O O       ",
+                 "03|           O O O O O O O O O O O O O O O O O O O O           ",
+                 "02|               O O O O O O O O O O O O O O O O               ",
+                 "01|                 O O O O O O O O O O O O O O O               ",
+                 "  x————————————————————————-------------------------------------",
+                 "    1 2 3 4 5 6 7 8 9 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 3 ",
+                 "                      0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 "};
+
+        while (true)
+        {
+            foreach (var test in screening_room)
+            {
+                Console.WriteLine(test);
+            }
+            Console.WriteLine("Selecteer een rij:");
+            var input_row = Convert.ToInt32(Console.ReadLine());
+            var row = (screening_room.Count - input_row - 3);
+            Console.WriteLine("Selecteer een stoel:");
+            var input_column = (Convert.ToInt32(Console.ReadLine()) - 1) * 2 + 4;
+            var your_line = screening_room[row];
+            if (your_line[input_column] == Convert.ToChar("O"))
+            {
+                your_line = your_line.Remove(input_column, 1).Insert(input_column, "X");
+                screening_room[row] = your_line;
+                Console.Clear();
+                Console.WriteLine($"U heeft stoel {input_column / 2 - 1} op rij {input_row} geselecteerd, wilt u deze stoel reserveren?");
+                Thread.Sleep(3000);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Deze stoel is niet beschikbaar, kies alstublieft een beschikbare stoel");
+                Thread.Sleep(3000);
+                Console.Clear();
+            }
+
         }
     }
 }
-
