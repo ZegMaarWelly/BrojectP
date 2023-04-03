8d4419038e782036ffafaf584891d459adaf3c17
1058989 <1058989@hr.nl>
2023-04-03 15:03:54 +0200

    added  a room model

---
+++ Project/DataModels/RoomModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+class RoomModel
+{
+    [JsonPropertyName("id")]
+    public int ID { get; set; }
+
+    [JsonPropertyName("map")]
+    public List<string> Map { get; set; }
+
+    [JsonPropertyName("total_seats")]
+    public int Total_Seats{ get; set; }
+
+    [JsonPropertyName("available_seats")]
+    public int Available_Seats { get; set; }
+
+    public RoomModel(int id, List<string> map, int total, int available)
+    {
+        ID = id;
+        Map = map;
+        Total_Seats = total;
+        Available_Seats = available;
+    }
+
+    public override string ToString()
+    {
+        return $"ID: {ID} \n {Map}\n Total Seats: {Total_Seats} \n Available Seats: {Available_Seats}";
+    }
+}
+
+
+
+
