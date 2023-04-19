2974d667efec762b609052e6b848d2d5526ea3b4
1058989 <1058989@hr.nl>
2023-04-19 11:13:59 +0200

    updated styling

--- Project/Presentation/AdminManageMovie.cs
+++ Project/Presentation/AdminManageMovie.cs
@@ -116,7 +116,7 @@ static class AdminManageMovie
         List<MovieListModel> movie_list = movieLogic.Return_Movie_List();
         foreach (MovieListModel movie in movie_list)
         {
-            Console.WriteLine(movie);
+            Console.WriteLine( $"ID: {movie.Id}| Name: {movie.Name}\n");
         }
     }
 
