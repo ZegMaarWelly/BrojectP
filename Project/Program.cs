2452011ce3b0262c491a8a60596dba7bc7a4fb68
RedShiftNinja <1064928@hr.nl>
2023-05-19 21:51:25 +0200

    nuking branch
    nuking branch

--- Project/Program.cs
+++
@@ -1,9 +0,0 @@
-
-Console.ForegroundColor = ConsoleColor.Magenta;
-Console.WriteLine("|__|. _ |_   /  . _  _ _  _   |__)_ |_|_ _ _ _| _  _  \r\n|  ||(_)| )  \\__|| )(-|||(_|  | \\(_)|_|_(-| (_|(_|||| \r\n     _/                                               ");
-Console.ResetColor();
-Console.WriteLine();
-
->>>>>>> Stashed changes
-Menu.Start();
-
